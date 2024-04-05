ced8a2ab684c91bc6eb7d55bf7146ec937d260f9
Wimple <w.umaoeng.477036@umindanao.edu.ph>
2024-04-05 21:34:51 +0800

    first commit

--- AIFCP/routes/web.php
+++ AIFCP/routes/web.php
@@ -19,15 +19,9 @@ Route::get('/', function () {
     return view('login');
 });
 
-//login
 Route::post('home',[CustomController::class,'welcome']);
 Route::get('home', [CustomController::class, 'welcome'])->name('home');
 Route::get('/manage', [CustomController::class, 'manage'])->name('manage');
-// Route::post('/login-user',[CustomAuthController::class,'loginUser'])->name('login-user');
 
-//register
-// Route::get('/register',[CustomAuthController::class,'registerUser']);
-// Route::get('/register', [CustomAuthController::class, 'register'])->name('register');
-// Route::post('/register', [CustomAuthController::class, 'registration'])->name('user.registration');
 
 
