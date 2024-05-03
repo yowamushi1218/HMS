0c5da4651acbd21bef1b0bc52cde8f69d7e653d4
Wimple <w.umaoeng.477036@umindanao.edu.ph>
2024-05-03 15:51:14 +0800

    first commit

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>HMS</title>
+<title>MedSysPro</title>
 <meta name="description" content="SMS">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" type="image/x-icon" href="{{ asset('assets/login/images/logo.png') }}">
